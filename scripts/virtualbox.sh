2ef32c74c6070162078a13003c3fa4b8d8e0c418
Christoph Hartmann <chris@lollyrock.com>
2014-03-05 15:32:55 +0100

    rename peewee user to packer user

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,8 +1,8 @@
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/veewee/.vbox_version)
 cd /tmp
-mount -o loop /home/veewee/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/packer/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/veewee/VBoxGuestAdditions_*.iso
+rm -rf /home/packer/VBoxGuestAdditions_*.iso
 
