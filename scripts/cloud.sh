bbcae410ec478bada20b2de6f4da3be385d19fc9
Christoph Hartmann <chris@lollyrock.com>
2014-03-10 16:25:31 +0100

    install haveged

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -4,3 +4,6 @@
 yum -y install perl python python-setuptools cloud-init python-pip
 pip-python install argparse 'boto==2.5.2' heat-cfntools
 cfn-create-aws-symlinks --source /usr/bin
+
+# Install haveged
+yum -y install haveged
