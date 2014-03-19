8b0f4502a9d354a1fd9f3322b176725dd1ea78d6
Christoph Hartmann <chris@lollyrock.com>
2014-03-19 10:56:29 +0100

    Improve cloud script for openstack - add serial console - configure eth0 and eth1 properly

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -1,6 +1,43 @@
 # to install the following packages, epel is required
+yum -y update
 
-# Installs cloudinit and heat-cfntools
-yum -y install perl python python-setuptools cloud-init python-pip
-pip-python install argparse 'boto==2.5.2' heat-cfntools
-cfn-create-aws-symlinks --source /usr/bin
+# Installs cloudinit
+yum -y install cloud-init 
+
+# Current version does not work well with the latest cloud-init version
+# Further testing is required
+# Install heat-cfntools
+#yum -y install python python-devel
+#wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+#python get-pip.py
+#yum -y install gcc
+#pip install heat-cfntools
+#cfn-create-aws-symlinks --source /usr/bin
+
+# Configure serial console
+
+#In order for nova console-log to work properly on CentOS 6.x
+# already done in 6.5
+# echo "serial --unit=0 --speed=115200"  >> /boot/grub/grub.conf
+# echo "terminal --timeout=10 console serial"  >> /boot/grub/grub.conf
+
+# Edit the kernel line to add the console entries
+# echo "kernel ... console=tty0 console=ttyS0,115200n8"  >> /boot/grub/menu.lst
+sed -i '/kernel/s|$| console=tty0 console=ttyS0,115200n8 |' /boot/grub/grub.conf
+
+
+# Disable the zeroconf route
+echo "NOZEROCONF=yes" >> /etc/sysconfig/network
+echo "PERSISTENT_DHCLIENT=yes" >> /etc/sysconfig/network
+
+# Configure network cards and remove device specific configuration
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+touch /etc/udev/rules.d/70-persistent-net.rules
+
+# remove uuid
+sed -i '/UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+# support second network card
+cp /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth1
+
+# remove password from root
+passwd -d root
